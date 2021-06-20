1e621d0b417b7b5319ee6ff1b9cbde1dd803a33f
Yifei Feng <fengyifei.528@bytedance.com>
2021-06-20 14:21:30 +0800

    add: test errors 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,4 +27,22 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+  testWidgets('Counter increments smoke test 2', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('10'), findsOneWidget);
+    expect(find.text('11'), findsNothing);
+  });
+
+  testWidgets('Counter increments smoke test 2', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('101'), findsOneWidget);
+    expect(find.text('12'), findsNothing);
+  });
 }
