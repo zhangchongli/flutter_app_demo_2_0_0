66ece115c17e81c2ac9d4122ddc83f8a54c65bad
Yifei Feng <fengyifei.528@bytedance.com>
2021-08-16 11:30:30 +0800

    ..

--- build_hotupdate.sh
+++ build_hotupdate.sh
@@ -1,7 +1,7 @@
 echo "1.构建宿主信息"
 git checkout "$1"
-flutter clean
-flutter build dynamic-host
+flutterw clean
+flutterw build dynamic-host
 
 echo "2.拷贝宿主信息"
 git reset --hard
@@ -10,5 +10,5 @@ cp build/dynamic/flutter_assets/diff.dill ./
 cp build/dynamic/flutter_assets/host_manifest.json ./
 
 echo "3.diff 出热更新包"
-flutter clean
-flutter build dynamic --hot-update --hot-update-host-dill=./diff.dill --hot-update-host-manifest=./host_manifest.json
+flutterw clean
+flutterw build dynamic --hot-update --hot-update-host-dill=./diff.dill --hot-update-host-manifest=./host_manifest.json
